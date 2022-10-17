0fd237a8c01c402e916e5bd1a20739fcecf8722a
Morganero <morganero1@gmail.com>
2022-10-17 14:41:15 -0500

    added locations to nav and location items

--- src/components/KandyKorner.js
+++ src/components/KandyKorner.js
@@ -5,22 +5,52 @@ import { NavBar } from "./nav/NavBar"
 import { Login } from "./auth/Login"
 import { Register } from "./auth/Register"
 import "./KandyKorner.css"
+import { useState, useEffect } from "react"
 
 
 export const KandyKorner = () => {
-	return <Routes>
-		<Route path="/login" element={<Login />} />
-		<Route path="/register" element={<Register />} />
 
-		<Route path="*" element={
-			<Authorized>
-				<>
-					<NavBar />
-					<ApplicationViews />
-				</>
-			</Authorized>
+const [products, setProducts] = useState([])
+const [productTypes, setProductTypes] = useState([])
 
-		} />
-	</Routes>
+
+useEffect(() => {
+	fetch('http://localhost:8088/products')
+	.then((response) => response.json())
+	.then((data) => { 
+		setProducts(data)
+	})
+}, []
+)
+useEffect(() => {
+	fetch('http://localhost:8088/productTypes')
+	.then((response) => response.json())
+	.then((data) => { 
+		setProductTypes(data)
+	})
+}, []
+)
+	
+
+
+	return (
+		<>
+		
+			<Routes>
+				<Route path="/login" element={<Login />} />
+				<Route path="/register" element={<Register />} />
+
+				<Route path="*" element={
+					<Authorized>
+						<>
+							<NavBar />
+							<ApplicationViews />
+						</>
+					</Authorized>
+
+				} />
+			</Routes>
+		</>
+	)
 }
 
--- src/components/views/ApplicationViews.js
+++ src/components/views/ApplicationViews.js
@@ -1,6 +1,18 @@
-export const ApplicationViews = () => {
-	return <>
+import { Outlet, Route, Routes } from "react-router-dom"
+import { LocationList } from "../locations/locationList"
 
-	</>
+
+export const ApplicationViews = () => {
+	return (
+	<Routes>
+
+		<Route path="/" element= {
+			<>
+			<h1>Kandy Korner</h1>
+			</>
+		} />
+		<Route path="locationList" element={ <LocationList /> } />
+	</Routes>
+	)
 }
 
